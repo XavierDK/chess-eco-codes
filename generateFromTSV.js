9470cc52674ae7e62a13d70142e1e2960b91a915
vjuneja <vanuj85@yahoo.co.in>
2020-03-13 01:30:48 -0700

    better fens

--- generateFromTSV.js
+++ generateFromTSV.js
@@ -1,4 +1,6 @@
 const fs = require('fs'),
+    path = require('path'),
+    Chess = require('chess.js').Chess,
     inputFiles = ["./eco/a.tsv",
                     "./eco/b.tsv",
                     "./eco/c.tsv",
@@ -12,17 +14,20 @@ inputFiles.forEach ((file) => {
     let inputLines = input.split("\n");
     inputLines.forEach((line)=>{
         let values = line.split("\t")
+
         if (values.length != 4 || values[0]==="eco") {
             return
         }
         const eco = values[0],
             name = values[1],
-            fen = values[2]
+            moves = values[3],
+            chess = new Chess()
+        chess.load_pgn(moves,{sloppy:true})
         let opening = {}
         opening.code = eco
         opening.name = name
-        codes[fen] = opening
+        codes[chess.fen()] = opening
     })
 })
 
-fs.writeFileSync("./codes.json", JSON.stringify(codes))
+fs.writeFileSync(path.join(__dirname, 'codes.json'), JSON.stringify(codes))
